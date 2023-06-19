bb57864c5ef472f1c3656ed72927db92a96ce10e
ronyderra <derrarony@gmail.com>
2023-06-19 14:09:13 +0300

    bitcoin code

--- src/sections/hero/hero.js
+++ src/sections/hero/hero.js
@@ -12,7 +12,7 @@ export const Hero = (props) => {
       Y tú puedes convertirte en el <span className="yellow-text">próximo millonario...</span>
       </h3>
       <div className="content-container">
-        <video controlsList="nodownload" preload="auto" className="video" controls loop>
+        <video controlsList="nodownload" preload="auto" className="video" controls loop muted autoPlay>
           <source src="https://bravoseguro.com/img/banner-6eb9e040b37cdcb23098c5d7b0b0e92d.mp4" type="video/mp4"/>
           Tu navegador no admite la etiqueta de video.
         </video>
