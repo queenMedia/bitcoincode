af94b40bf389c5d38445f2da850d0c165d66b6d8
ronyderra <derrarony@gmail.com>
2023-07-03 11:37:45 +0300

    header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,11 @@
 import "./header.css";
 import "./header.responsive.css";
-import logo from "../../../src/assets/images/logo.png";
+import logo from "../../../src/assets/bitcoin360ai/7222d243862daf533fdbbeabaeb8f8f9.png";
 export const Header = (props) => {
   return (
     <header className="header">
         <img src={logo} alt="logo" className="image" />
+        <button>asdfasdf</button>
     </header>
   );
 };
