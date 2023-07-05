dfbec749dd380d2e105fe6b7532f220b9ea6b4c8
ronyderra <derrarony@gmail.com>
2023-07-05 16:20:38 +0300

    header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import { Hero } from "./sections/hero/hero.js";
 import { Invest } from "./sections/invest/invest.js";
 import { Platform } from "./sections/platform/platform.js";
 import { About } from "./sections/about/about.js";
-import { Works } from "./sections/works/works.js";
-import { Actions } from "./sections/actions/actions";
-import { Faqs } from "./sections/faq/faqs";
-import { Footer } from "../src/sections/footer/footer";
 import Carousel from "../src/sections/carousel/carousel";
 
 function App() {
