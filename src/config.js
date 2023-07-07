3dc140971f9afde6dd51669bda4008a3d912121f
Ricardo Jimenez <ricardojimenez@pop-os.localdomain>
2023-07-07 09:56:21 -0600

    FEAT: country code

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const phonecode = "51";
-export const country = "PE";
+export const phonecode = "351";
+export const country = "PT";
 export const landing = "bitcoin-code";
 export const idpass = "123qwe321";
 export const whitePage = "https://www.martinviz.com/article/martin-vizcarra-asegura-dejar-la-presidencia-de-peru-con-la-frente-en-alto/thanku";
